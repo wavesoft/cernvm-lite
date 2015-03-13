d4bf53b3146f93b8f8612b6ac118d2fbb3eb9f55
wavesoft <wavesoft@github.com>
2015-03-13 15:20:32 +0100

    Simplifying RW mount

--- boot-scripts/cernvm-iboot.sh
+++ boot-scripts/cernvm-iboot.sh
@@ -76,11 +76,7 @@ function MACRO_RO {
 }
 # Create writable directory in $1
 function MACRO_RW {
-	TMP_DIR="${GUEST_CACHE_RW}/$1"
-	mkdir -p "${TMP_DIR}"
-	[ !-d "${GUEST_ROOT}/$1" ] && mkdir -p "${GUEST_ROOT}/$1"
-	mount --bind "${TMP_DIR}" "${GUEST_ROOT}/$1"
-	#ln -s "${TMP_DIR}" "${GUEST_ROOT}/$1"
+	mkdir -p ${GUEST_ROOT}/$1
 }
 # Create directoriy in $1
 function MACRO_MKDIR {
